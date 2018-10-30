348ac3dfbe38781a4016576a240e12e914294db3
anonymous-ME <affanahmadfahmi@gmail.com>
2018-10-30 23:17:07 +0530

    Adding Configuration Options

--- project.py
+++ project.py
@@ -2,17 +2,26 @@ import mysql.connector
 import re
 import requests
 import json
+import ConfigParser
 
-slack_url = "https://hooks.slack.com/services/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"
-WarningThreshold=90
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'auto_increment_check.config'))
+user_name = config.get('MySQL', 'user_name')
+password = config.get('MySQL', 'password')
+host = config.get('MySQL', 'host')
+database = config.get('MySQL', 'database')
+slack_url = config.get('Slack', 'msg_url')
+WarningThreshold = config.get('Param', 'Warning_Threshold')
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  database = "project",
-  user="root",
-  passwd="password"
+  host= host,
+  database = database,
+  user = user_name,
+  passwd = password
 )
+
 Limits={'tinyint':127,'tinyint unsigned':255,'smallint':32767,'smallint unsigned':65535,'mediumint':8388607,'mediumint unsigned':16777215,'int':2147483647,'int unsigned':4294967295,'bigint':9223372036854775807,'bigint unsigned':18446744073709551615}
+
 mycursor = mydb.cursor()
 
 mycursor.execute("show tables")
@@ -28,6 +37,6 @@ for x in myresult:
 
     percent_full = (int(curr_val[0][0])/int(max_val))*100
     
-    if percent_full >= WarningThreshold:
+    if int(percent_full) >= int(WarningThreshold):
         msg = {"text":"**WARNING** \n Auto Increment LIMIT APPROACHING for "+str(x[0])+" table."}
         requests.post(slack_url,data=json.dumps(msg))
